52b8390353ad45a8ddaf48baf06ed5dd82b52770
jakubiros <116426972+jakubiros@users.noreply.github.com>
2025-01-06 20:12:12 +0100

    Added visits counter

--- app/app.py
+++ app/app.py
@@ -18,5 +18,13 @@ def server_time():
     now = datetime.now()
     return f"Current server time is: {now.strftime('%Y-%m-%d %H:%M:%S')}"
 
+visits = 0
+
+@app.route('/visits')
+def visit_counter():
+    global visits
+    visits += 1
+    return f"This page has been visited {visits} times."
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000,debug=True)
